b8be7c01ef6e14075e1a472dd873991e29faa727
bk24z <bradsthedeveloper@gmail.com>
2025-12-07 14:28:19 +0000

    Rename calculate function variables in day7_part2.py for clarity

--- AoC2025/day7/day7_part2.py
+++ AoC2025/day7/day7_part2.py
@@ -11,13 +11,13 @@ def main():
     start_col_idx = grid[0].index("S")
 
     @cache
-    def calculate(row, col):
-        if row == last_row_idx:
+    def calculate(row_idx, col_idx):
+        if row_idx == last_row_idx:
             return 1
-        if grid[row + 1][col] == ".":
-            return calculate(row + 1, col)
-        if grid[row + 1][col] == "^":
-            return calculate(row + 1, col - 1) + calculate(row + 1, col + 1)
+        if grid[row_idx + 1][col_idx] == ".":
+            return calculate(row_idx + 1, col_idx)
+        if grid[row_idx + 1][col_idx] == "^":
+            return calculate(row_idx + 1, col_idx - 1) + calculate(row_idx + 1, col_idx + 1)
         return None
 
     print(calculate(0, start_col_idx))
